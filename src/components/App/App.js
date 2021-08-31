c8d449da6b128dbaf3e4aa2a437bb180adf44f39
Maciev <apcpc951995@gmail.com>
2021-08-31 19:03:43 -0400

    adding entities and connections

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,27 @@ import React from "react";
 import { Hero, Header, IconBar, Checkout } from "components";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AppProvider } from "context/AppProvider";
+import "reflect-metadata";
+
+import { createConnection } from "typeorm";
+import { User } from "../../db/typeorm/entities/userEntity";
+import { Product } from "../../db/typeorm/entities/productEntity";
+
+createConnection({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "admin",
+  database: "test",
+  entities: [Product, User],
+  synchronize: true,
+  logging: false,
+})
+  .then((connection) => {
+    // here you can start to work with your entities
+  })
+  .catch((error) => console.log(error));
 
 export const App = () => {
   return (
