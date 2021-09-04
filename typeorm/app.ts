71b5045434c89963ebbadf12a494667a55f31434
Maciev <apcpc951995@gmail.com>
2021-09-03 23:30:13 -0400

    express working

--- typeorm/app.ts
+++ typeorm/app.ts
@@ -34,3 +34,5 @@ createConnection()
     app.listen(3000, () => console.log("App is listening on port 3000!"));
   })
   .catch((error) => console.log(error));
+
+//let connectionModule = module.exports = { connectionModule };
